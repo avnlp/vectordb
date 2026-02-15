a600a2296ad2ffad1df2fddad821e14ea17a6455
vrunm <97465624+vrunm@users.noreply.github.com>
2026-02-16 01:08:45 +0530

    Update key

--- src/vectordb/langchain/dense_indexing/weaviate_dense_indexing.py
+++ src/vectordb/langchain/dense_indexing/weaviate_dense_indexing.py
@@ -72,7 +72,7 @@ def main():
 
     generator = ChatGroqGenerator(
         model="llama-3.1-8b-instant",
-        api_key="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",
+        api_key="GROQ_API_KEY",
         llm_params={
             "temperature": 0,
             "max_tokens": 1024,
